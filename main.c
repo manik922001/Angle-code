4cc14c851501b935ebcb8cb8146b96189ed71aec
manik922001 <57584417+manik922001@users.noreply.github.com>
2019-11-23 00:20:11 +0530

    initial comit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a,b,c;
+    printf("enter 2 angles");
+    scanf("%f%f",&a,&b);
+    c=180-(a+b);
+    printf("the 3rd angle is %f",c);
+    return 0;
+}
